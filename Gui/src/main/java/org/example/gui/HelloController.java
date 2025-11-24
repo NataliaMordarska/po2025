5a1f8281fa623b166c5dfdf76ea9cd59d16d8e18
Natalia <nmordars@student.agh.edu.pl>
2025-11-24 20:01:07 +0100

    fxml

--- Gui/src/main/java/org/example/gui/HelloController.java
+++ Gui/src/main/java/org/example/gui/HelloController.java
@@ -1,14 +1,46 @@
 package org.example.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
+    private Button startButton;
+
+    @FXML
+    private Button stopButton;
+
+    @FXML
+    private Button increaseGearButton;
+
+    @FXML
+    private Button decreaseGearButton;
+
+    @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onStartButton() {
+        System.out.println("Samochód uruchomiony!");
+        welcomeText.setText("Samochód uruchomiony!");
+    }
+
+    @FXML
+    private void onStopButton() {
+        System.out.println("Samochód zatrzymany!");
+        welcomeText.setText("Samochód zatrzymany!");
+    }
+
+    @FXML
+    private void onIncreaseGearButton() {
+        System.out.println("Zwiększono bieg!");
+        welcomeText.setText("Zwiększono bieg!");
+    }
+
+    @FXML
+    private void onDecreaseGearButton() {
+        System.out.println("Zmniejszono bieg!");
+        welcomeText.setText("Zmniejszono bieg!");
     }
 }
